1f41af4c4343043aef9702cf785d4a2a79ffbc0c
Jericho <mackulinm@gmail.com>
2023-04-15 20:50:43 +0200

    Update LogExtensions.cs

--- scripts/util/LogExtensions.cs
+++ scripts/util/LogExtensions.cs
@@ -9,9 +9,29 @@ public static class LogExtensions
         Debug.Log(message, gameObject);
     }
     
+    public static void LogWarning(this GameObject gameObject, string message)
+    {
+        Debug.LogWarning(message, gameObject);
+    }
+
+    public static void LogError(this GameObject gameObject, string message)
+    {
+        Debug.LogError(message, gameObject);
+    }
+    
     public static void Log(this Transform transform, string message)
     {
         Debug.Log(message, transform);
+    }
+    
+    public static void LogWarning(this Transform transform, string message)
+    {
+        Debug.LogWarning(message, transform);
+    }
+    
+    public static void LogError(this Transform transform, string message)
+    {
+        Debug.LogError(message, transform);
     }
 }
 }
