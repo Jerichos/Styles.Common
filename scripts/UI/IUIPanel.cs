ac5621b3873fb0f34d66a08f07d02f6ccbe6df98
Jericho <mackulinm@gmail.com>
2023-04-15 20:27:15 +0200

    Add common scripts for UI panels

---
+++ scripts/UI/IUIPanel.cs
@@ -0,0 +1,9 @@
+﻿namespace Styles.Common
+{
+public interface IUIPanel
+{
+    void Open();
+    void Close();
+    void Toggle();
+}
+}
---
+++ scripts/UI/UIPanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Styles.Common
+{
+public class UIPanel : MonoBehaviour, IUIPanel
+{
+    public virtual void Open()
+    {
+        gameObject.SetActive(true);
+        OnOpen();
+    }
+
+    public virtual  void Close()
+    {
+        gameObject.SetActive(false);
+        OnClose();
+    }
+
+    public virtual void Toggle()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+        
+        if(gameObject.activeSelf)
+            OnOpen();
+        else
+            OnClose();
+    }
+
+    protected virtual void OnOpen()
+    {
+        
+    }
+
+    protected virtual void OnClose()
+    {
+        
+    }
+}
+}
