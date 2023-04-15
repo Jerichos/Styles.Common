fc7a1d50e10c5285bc744bc27649e0194e8c2786
Jericho <mackulinm@gmail.com>
2023-04-15 20:32:12 +0200

    add log extensions

---
+++ scripts/util/LogExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Styles.Common.util
+{
+public static class LogExtensions
+{
+    public static void Log(this GameObject gameObject, string message)
+    {
+        Debug.Log(message, gameObject);
+    }
+    
+    public static void Log(this Transform transform, string message)
+    {
+        Debug.Log(message, transform);
+    }
+}
+}
