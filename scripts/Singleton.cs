14069696f47dad1f7e636bbaf73fc0c6a9bf2ab9
Jericho <mackulinm@gmail.com>
2023-04-15 20:27:50 +0200

    Add singleton base script

---
+++ scripts/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace Styles.Common
+{
+public abstract class Singleton<T> : MonoBehaviour where T : Behaviour
+{
+    [SerializeField] private bool _dontDestroyOnLoad;
+    
+    private T _instance;
+
+    public T Instance
+    {
+        get
+        {
+            if (_instance)
+                return _instance;
+            
+            Debug.LogError("singleton instance does not exists!");
+            return null;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(_instance.gameObject);
+            return;
+        }
+
+        _instance = GetComponent<T>();
+        
+        if(_dontDestroyOnLoad)
+            DontDestroyOnLoad(gameObject);
+    }
+    
+    
+}
+}
